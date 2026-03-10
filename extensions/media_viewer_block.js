e95e766618a7db2a9c5ef29e07f3f9c9d28f3c22
kawa-nobu <kwnobu@gmail.com>
2026-03-11 02:11:12 +0900

    Alt(Option)が押されている場合はメディアビューワーの動作を止めるようにした

--- extensions/media_viewer_block.js
+++ extensions/media_viewer_block.js
@@ -14,6 +14,26 @@ class OpdMediaViewerBlocker {
                     composed: true,
                     detail: JSON.stringify({ token: this.opd_send_media_info_token })
                 }));
+
+                /*ショートカットキーとしてAlt(Option)キーの動作を設定*/
+                document.addEventListener('keydown', (event) => {
+                    if(event.key === 'Alt'){
+                        column_window.document.dispatchEvent(new CustomEvent('opd_media_viewer_shotcut', {
+                            bubbles: true,
+                            composed: true,
+                            detail: JSON.stringify({token: this.opd_send_media_info_token, keys:{alt:true}})
+                        }));
+                    }
+                });
+                document.addEventListener('keyup', (event) => {
+                    if(event.key === 'Alt'){
+                        column_window.document.dispatchEvent(new CustomEvent('opd_media_viewer_shotcut', {
+                            bubbles: true,
+                            composed: true,
+                            detail: JSON.stringify({token: this.opd_send_media_info_token, keys:{alt:false}})
+                        }));
+                    }
+                });
             });
 
             column_window.document.head.appendChild(helper_script);
