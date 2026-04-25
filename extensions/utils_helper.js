90240aa96390703275011072dc975657dcccbac1
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2026-04-26 03:16:01 +0900

    ユーティリティのヘルパーを仕込めるようにした

--- extensions/utils_helper.js
+++ extensions/utils_helper.js
@@ -4,16 +4,18 @@
     /* ポスト画面などテキスト入力フォーカス状態を送信するようにする関数 */
     function sendTextFocusState(){
         //フォーカスされた時
-        document.addEventListener('focusin', (e) => {
+        window.addEventListener('focusin', (e) => {
             if (e.target.getAttribute('contenteditable') === 'true' || e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') {
                 console.log("focus")
                 window.dispatchEvent(new CustomEvent('opd_post_focus', {detail: true}));
             }
         });
         //フォーカスが外れた時
-        document.addEventListener('focusout', (e) => {
+        window.addEventListener('focusout', (e) => {
             console.log("focus-out")
             window.dispatchEvent(new CustomEvent('opd_post_focus', {detail: false}));
         });
     }
+
+    sendTextFocusState();
 })();
