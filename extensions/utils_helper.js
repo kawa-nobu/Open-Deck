a0e9cd26d2f03cacd6373a2c6fcb3f439d2aac75
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2026-04-25 20:14:20 +0900

    ユーティリティを追加

---
+++ extensions/utils_helper.js
@@ -0,0 +1,19 @@
+/* カラム内で使う細々とした機能を実装する */
+(() => {
+    console.log("UtilsWorking")
+    /* ポスト画面などテキスト入力フォーカス状態を送信するようにする関数 */
+    function sendTextFocusState(){
+        //フォーカスされた時
+        document.addEventListener('focusin', (e) => {
+            if (e.target.getAttribute('contenteditable') === 'true' || e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') {
+                console.log("focus")
+                window.dispatchEvent(new CustomEvent('opd_post_focus', {detail: true}));
+            }
+        });
+        //フォーカスが外れた時
+        document.addEventListener('focusout', (e) => {
+            console.log("focus-out")
+            window.dispatchEvent(new CustomEvent('opd_post_focus', {detail: false}));
+        });
+    }
+})();
