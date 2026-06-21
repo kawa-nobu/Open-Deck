dc3cf2afbe9b037606899d31d25a8519197c83e6
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2026-06-21 15:02:08 +0900

    scrollIntoViewとfocusが必要なユーザー操作場面では無効化を解除するようにした
    scrollIntoViewの挙動が横スクロールにも伝搬していたので、その部分も考慮して修正した

--- extensions/auto_reload_helper.js
+++ extensions/auto_reload_helper.js
@@ -3,17 +3,49 @@
     let path_old = null;
     let opd_reload_token = null;
     let reload_func = ()=>{};
+    let isFocusDisabled = false;
+    
+    // ユーザー操作でフォーカス無効化を解除する
+    ['mousedown', 'keydown', 'touchstart'].forEach(type => {
+        document.addEventListener(type, () => {
+            isFocusDisabled = false;
+        }, { capture: true, passive: true });
+    });
 
-    // 自動更新時にフォーカスされる問題があるので、scrollIntoViewとfocusを無効化する
+    // 自動更新時にフォーカスされる問題があるので、scrollIntoViewとfocusを一時的に無効化する
     const originalScrollIntoView = HTMLElement.prototype.scrollIntoView;
     HTMLElement.prototype.scrollIntoView = function(options) {
-        //上へのスクロールはOpen-Deck側で制御するので無効化する
-        return;
+        //フォーカス無効化が有効だった場合はフォーカスを無視する
+        if (isFocusDisabled) return;
+
+        // カラム側のスクロールが親の横スクロールにも伝搬する問題を以下で対処する
+        // 直近のスクロール可能な親要素を探す
+        let parent = this.parentElement;
+        while (parent && !/(auto|scroll)/.test(getComputedStyle(parent).overflow)) {
+            parent = parent.parentElement;
+        }
+        if (!parent) return;
+
+        // 対象要素とスクロール親の位置差分を取得してスクロールする
+        const client_rect = this.getBoundingClientRect();
+        const parent_client_rect = parent.getBoundingClientRect();
+
+        // はみ出し量を算出
+        const delta = (start, end, pStart, pEnd) =>
+            start < pStart ? start - pStart : end > pEnd ? end - pEnd : 0;
+
+        parent.scrollBy({
+            left: delta(client_rect.left, client_rect.right, parent_client_rect.left, parent_client_rect.right),
+            top: delta(client_rect.top, client_rect.bottom, parent_client_rect.top, parent_client_rect.bottom),
+            behavior: options?.behavior
+        });
     };
 
     const originalFocus = HTMLElement.prototype.focus;
     HTMLElement.prototype.focus = function(options) {
-        //自動更新が必要なカラム内ではフォーカスが不要なので無効化する
+        if (isFocusDisabled){
+            return;
+        }
         return originalFocus.call(this, Object.assign({}, options, { preventScroll: true }));
     };
 
@@ -60,6 +92,7 @@
         if(typeof reload_func !== 'function') return;
 
         try {
+            isFocusDisabled = true;
             reload_func();
         } catch (err) {
             console.warn('reload_func threw->', err);
